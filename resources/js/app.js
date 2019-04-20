2596d958b6352fd603389d3a5afd7469b4f850a7
Jonathon Ringeisen <ringeisen85@gmail.com>
2019-04-19 20:37:49 -1000

    Added fullcalendar.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router'
 import routes from './routes'
+import FullCalendar from 'vue-full-calendar'
 
 require('./bootstrap');
 
 window.Vue = require('vue');
 
 Vue.use(VueRouter)
+Vue.use(FullCalendar)
 
 let app = new Vue({
     el: '#app',
--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,15 +1,53 @@
 <template>
   <div class="card">
-      <div class="card-header">Home</div>
+      <div class="card-header">
+        Home
+        <button class="btn btn-sm btn-secondary float-right"><i class="fa fa-plus"></i> Add Item</button>
+      </div>
 
       <div class="card-body">
-          This is the homepage!
+          <full-calendar 
+            :events="events"
+            :config="config"></full-calendar>
       </div>
   </div>
 </template>
 
 <script>
+import 'fullcalendar/dist/fullcalendar.css'
+
 export default {
-  
+  data() {
+    return {
+      config: {
+        defaultView: 'month',
+        editable: false,
+        height: 700
+      },
+      events: [
+        {
+            title  : 'Item 1',
+            start  : '2019-04-19',
+            textEscape: false
+        },
+        {
+            title  : 'Item 2',
+            start  : '2019-04-19',
+        },
+        {
+            title  : 'Item 3',
+            start  : '2019-04-19',
+        },
+        {
+            title  : 'Item 4',
+            start  : '2019-04-19',
+        },
+        {
+            title  : 'Item 5',
+            start  : '2019-04-19',
+        },
+      ]
+    }
+  }
 };
 </script>
